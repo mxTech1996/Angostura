c1a219743c1ef8994beb9b483595fa978ea0a374
juliotwo <juliotwo1996@gmail.com>
2024-12-19 00:10:41 -0600

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
   const router = useRouter();
   const { dataSite } = useInformation();
 
-  console.log("env",process.env.NEXT_PUBLIC_API_KEY);
+  console.log('env', process.env.NEXT_PUBLIC_API_KEY);
   return (
     <main
       style={{
@@ -28,97 +28,95 @@ export default function Home() {
       <Navbar />
       <div className='relative'>
         <Hero
-          variant='background-img'
+          variant='img-left'
           src={dataSite.image_hero}
           colorText='#FCFCFCFF'
           title={dataSite.subtitle}
           description={dataSite.description}
-          srcSecondary={dataSite.image_hero2}
-          images={[dataSite.image_hero, dataSite.image_hero2]}
+          // images={[dataSite.image_hero, dataSite.image_hero2]}
           styleTextSecondSection={{
             color: 'black',
           }}
           withShadowText
+          stylesContainerImage={{
+            padding: '40px',
+          }}
+          styleImage={{
+            borderRadius: 2,
+          }}
         />
       </div>
-      <div className='container mx-auto flex flex-col gap-20 my-24'>
-        <div
-          style={{ zIndex: 2 }}
-          className='flex flex-col px-32'
-          id='our-services'
+      <div className='container mx-auto flex flex-col gap-20 my-24 mt-10'>
+        <FeaturesV2
+          gridColumns={2}
+          variant='text'
+          textColor='#000'
+          version='v2'
+          features={dataSite.services.map((service) => ({
+            ...service,
+            src: service.image,
+          }))}
+        />
+      </div>
+      <div style={{ zIndex: 2 }} className='flex flex-col px-48' id='know-us'>
+        <Typography.Title
+          level={3}
+          className='font-medium mb-10 text-center text-black'
         >
-          <Typography.Title
-            level={3}
-            className='font-medium mb-10 text-center text-black'
-          >
-            Our Services
-          </Typography.Title>
-          <Features
+          Know Us
+        </Typography.Title>
+        <Missions
+          textColor='#000'
+          data={dataSite.info}
+          gridColumns={1}
+          backgroundColor={'#E5D48FFF'}
+          variant='text'
+        />
+      </div>
+      <div id='courses' className='mt-20 px-20'>
+        {dataSite.products.length > 1 && (
+          <ProductSection
+            withCategoryFilter={false}
+            title='All Courses'
             gridColumns={2}
-            variant='text'
-            textColor='#000'
-            features={dataSite.services}
-          />
-        </div>
-        <div style={{ zIndex: 2 }} className='flex flex-col px-48' id='know-us'>
-          <Typography.Title
-            level={3}
-            className='font-medium mb-10 text-center text-black'
-          >
-            Know Us
-          </Typography.Title>
-          <Missions
-            textColor='#000'
-            data={dataSite.info}
-            gridColumns={3}
-            variant='card'
-          />
-        </div>
-        <div id='courses'>
-          {dataSite.products.length > 1 && (
-            <ProductSection
-              withCategoryFilter={false}
-              title='All Courses'
-              gridColumns={4}
-              variant='grid'
-              productItemVariant='vertical'
-              onClickImage={(id) => {
-                router.push(`/product/${id}`);
-              }}
-              productVersion='2'
-              carouselOptions={{
-                arrowColor: 'black',
-                fade: true,
-                autoPlay: false,
-                direction: 'horizontal',
-              }}
-              backgroundItemColor='#FBFBFB'
-              stylesItem={{
-                borderRadius: 12,
-              }}
-            />
-          )}
-        </div>
-
-        <div className='flex flex-col' id='references'>
-          <Typography.Title level={3} className='font-medium mb-10 text-center'>
-            References
-          </Typography.Title>
-          <References
+            variant='grid'
+            productItemVariant='vertical'
+            onClickImage={(id) => {
+              router.push(`/product/${id}`);
+            }}
+            productVersion='1'
             carouselOptions={{
               arrowColor: 'black',
               fade: true,
               autoPlay: false,
               direction: 'horizontal',
             }}
-            variantItem='card'
-            variant='grid'
-            backgroundColor='#CFE5BAFF'
-            references={dataSite.references}
-            gridColumns={3}
-            titleAlign='center'
+            backgroundItemColor='#FBFBFB'
+            stylesItem={{
+              borderRadius: 12,
+            }}
           />
-        </div>
+        )}
+      </div>
+
+      <div className='flex flex-col px-20' id='references'>
+        <Typography.Title level={3} className='font-medium mb-10 text-center'>
+          References
+        </Typography.Title>
+        <References
+          carouselOptions={{
+            arrowColor: 'black',
+            fade: true,
+            autoPlay: false,
+            direction: 'horizontal',
+          }}
+          variantItem='card'
+          variant='carousel'
+          backgroundColor='#E5D7BAFF'
+          references={dataSite.references}
+          gridColumns={1}
+          titleAlign='center'
+        />
       </div>
 
       <Footer />
