acf3cba09a2d70cf48504b2aa9a3e115fb797f03
juliotwo <juliotwo1996@gmail.com>
2025-07-18 15:10:24 -0600

    fix

--- src/data/index.js
+++ src/data/index.js
@@ -40,3 +40,242 @@ export const footerData = [
     label: 'Terms',
   },
 ];
+export const dataSite = {
+  name: 'Angostura',
+  url: 'https://laangostura.lat/',
+  telephone: '5535179989',
+  email: 'admlaangostura@emailpro.pro',
+  address:
+    'LOMAS DEL VALLE 430, LOCAL 2 AL 7, LOMAS DEL VALLE, SAN PEDRO GARZA GARCIA, NUEVO LEON, C.P. 66256,MEXICO.',
+  description:
+    'We specialize in providing comprehensive consulting services in computing and information technology,Our team of experienced professionals is dedicated to helping businesses navigate the complexities of technology to drive efficiency and innovation.',
+  subtitle: 'Your Partner in Navigating the Digital Landscape',
+  image_hero:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/ag_1_.jpg',
+  image_hero2:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/ag2_.jpg',
+  iconImage:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/logo-no-background_guU76gv.png',
+  info: [
+    {
+      title: 'Vision',
+      description:
+        'To be a leading provider of innovative IT consulting services, empowering businesses to achieve their full potential through the strategic use of technology.',
+    },
+    {
+      title: 'Mission',
+      description:
+        'Our mission is to deliver tailored IT consulting solutions that enhance efficiency, drive innovation, and foster sustainable growth for our clients,We strive to build long-lasting partnerships based on trust, transparency, and exceptional service.',
+    },
+    {
+      title: 'Values',
+      description:
+        'Integrity,we uphold the highest standards of honesty and ethical conduct in all our interactions.\r\nCustomer Focus, Our clients are at the heart of everything we do. We listen to their needs and provide customized solutions that deliver results, Excellence,we are committed to excellence in our services, ensuring quality and continuous improvement in all our processes,Innovation, We embrace change and encourage creative thinking to provide cutting-edge solutions that meet the evolving needs of our clients.',
+    },
+  ],
+  services: [
+    {
+      title: 'IT Infrastructure Assessment',
+      description:
+        'Evaluate and optimize your current IT infrastructure to ensure it meets your business needs and supports future growth.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/ag_3.jpg',
+    },
+    {
+      title: 'Cybersecurity Consulting',
+      description:
+        'Implement comprehensive cybersecurity strategies to protect your business from threats, including risk assessments, vulnerability testing, and incident response planning.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/ag_4.jpg',
+    },
+    {
+      title: 'Cloud Solutions',
+      description:
+        'Assist businesses in migrating to cloud platforms, providing setup, management, and support for cloud services tailored to enhance scalability and efficiency.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/ag_5_.jpg',
+    },
+    {
+      title: 'Data Management and Analytics',
+      description:
+        'Develop data management strategies and analytics solutions that enable businesses to harness their data for actionable insights and informed decision-making.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/ag_6.jpg',
+    },
+    {
+      title: 'Custom Software Development',
+      description:
+        'Design and develop bespoke software applications that address specific business challenges and streamline operations, ensuring better alignment with organizational goals.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/ag_7.jpg',
+    },
+  ],
+  references: [
+    {
+      name: 'Samuel Diaz',
+      description:
+        'We hired this consulting firm to upgrade our company’s infrastructure and implement cloud services. Their team was professional, responsive, and delivered on time.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Mariana Humilton',
+      description:
+        'The consultants from this firm are incredibly knowledgeable,They helped us resolve long-standing issues with our network security and implemented cutting-edge cybersecurity measures.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Angelica Rodriguez',
+      description:
+        'Our company faced repeated technical issues that were costing us time and resources,After bringing in this team, they quickly identified the problem areas and provided effective solutions.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Manuel Farias',
+      description:
+        'We’ve worked with several IT consulting firms in the past, but this one stands out for their personalized approach and deep industry knowledge,They helped us transition to new software and trained our staff.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Raul Duarte',
+      description:
+        'This consulting team exceeded our expectations,They communicated clearly throughout the entire project, keeping us informed every step of the way.',
+      rating: 5,
+      image: null,
+    },
+  ],
+  products: [
+    {
+      id: 430,
+      name: 'IT Strategy Consulting',
+      category: null,
+      description:
+        'Is a service-oriented product aimed at organizations seeking to optimize their IT operations and align their technology initiatives with business goals',
+      price: '788.99',
+      stock: 100,
+      content:
+        "Target Audience,Designed for businesses of all sizes, including small and medium enterprises (SMEs) and large corporations, looking to improve their IT strategy and implementation,Comprehensive Assessment,Involves a thorough assessment of an organization's current IT infrastructure, systems, processes, and strategies to identify areas for improvement.\r\nStrategic Alignment,Focuses on aligning IT initiatives with business objectives, ensuring that technology investments support the overall goals of the organization.",
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pexels-markusspiske-225769.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 431,
+      name: 'Cloud Solutions',
+      category: null,
+      description:
+        'Refers to a range of services and technologies designed to enable organizations to store, manage, and process data and applications over the internet, rather than on local servers or personal computers',
+      price: '1399.99',
+      stock: 100,
+      content:
+        'Target Audience,Designed for businesses of all sizes, from startups to large enterprises, looking to enhance their IT infrastructure, improve scalability, and reduce operational costs,Types of Cloud Services,Typically includes various models such as,Infrastructure as a Service (IaaS), Provides virtualized computing resources over the internet,Platform as a Service (PaaS),Offers a platform allowing customers to develop, run, and manage applications without dealing with infrastructure management.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pexels-divinetechygirl-1181472.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 432,
+      name: 'Cybersecurity Services',
+      category: null,
+      description:
+        'Incompasses a variety of strategies, tools, and solutions designed to protect organizations from cyber threats and ensure the security of their data and IT infrastructure.',
+      price: '900.99',
+      stock: 100,
+      content:
+        'Target Audience,Aimed at businesses of all sizes, government agencies, and non-profit organizations seeking to enhance their cybersecurity posture and protect sensitive information.\r\nService Offerings: Typically includes a range of services such as, Risk Assessment,Evaluating the organization’s current security posture and identifying vulnerabilities.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/pexels-thisisengineering-3862373.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 433,
+      name: 'Data Analytics and Business Intelligence',
+      category: null,
+      description:
+        'Data Analytics and Business Intelligence" (BI) refers to a range of technologies, tools, and practices used to collect, analyze, and present business data to support informed decision-making.',
+      price: '1499.99',
+      stock: 100,
+      content:
+        'Data Source, Utilizes various data sources, including internal databases, customer data, market trends, and external datasets to provide comprehensive insights,Tools and Technologies,Often involves the use of specialized software and tools for data visualization, reporting, and analysis. Common tools .',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/ang_3.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 434,
+      name: 'Software Development and Integration',
+      category: null,
+      description:
+        'Software Development and Integration refers to the process of designing, creating, and implementing software applications and systems, along with integrating them with existing technology infrastructures.',
+      price: '1799.99',
+      stock: 100,
+      content:
+        'Target Audience,Aimed at businesses of all sizes, including startups, enterprises, and organizations looking to develop custom software solutions or integrate third-party applications into their existing systems,Development Methodologies,Utilizes various software development methodologies, including Agile, Scrum, Waterfall, and DevOps, to guide the development process and enhance collaboration.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/ang_4.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 435,
+      name: 'IT Project Management',
+      category: null,
+      description:
+        'Encompasses the planning, execution, and oversight of information technology projects to ensure they are completed on time, within scope, and on budget',
+      price: '1899.99',
+      stock: 100,
+      content:
+        'Target Audience,Designed for IT project managers, team leaders, and organizations looking to implement efficient project management practices within their IT departments,Project Lifecycle, Covers the entire project lifecycle, which typically includes, Initiation,Defining the project’s purpose, goals, and feasibility, Planning,Developing detailed project plans, including scope, timelines, resources, and budgets.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/ang_5_.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 436,
+      name: 'Digital Transformation Consulting',
+      category: null,
+      description:
+        'is a service that helps organizations leverage digital technologies to fundamentally change how they operate and deliver value to customers',
+      price: '1499.99',
+      stock: 100,
+      content:
+        "Comprehensive Assessment,Involves a thorough analysis of the organization's current digital capabilities, including technology infrastructure, processes, and organizational culture,Strategic Roadmap: Develops a tailored digital transformation strategy that outlines specific goals, initiatives, and timelines for implementing digital technologies and processes.",
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/an_g_6_.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 437,
+      name: 'Training and Support Services',
+      category: null,
+      description:
+        'Refer to a suite of offerings designed to enhance the skills of users and ensure the effective use of products, systems, or technologies within an organization.',
+      price: '1799.99',
+      stock: 100,
+      content:
+        'Product Knowledge,Ensures users understand the features and functionalities of specific products or technologies, promoting effective utilization,Skill Development, Enhances user skills through hands-on training, workshops, and e-learning courses, enabling them to perform their jobs more effectively,Process Improvement, Trains users on optimized workflows and processes to enhance productivity and efficiency.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/ang_6.jpg',
+      image2: null,
+      image3: null,
+    },
+  ],
+  categories: [],
+};
